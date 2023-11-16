9f6091df2cf0c6701618c20f9c6035771513931d
agusgermi <agerminario@fi.uba.ar>
2023-11-16 16:59:54 -0300

    Creamos clase jugador

---
+++ src/main/java/edu/fiuba/algo3/modelo/Jugador.java
@@ -0,0 +1,13 @@
+package edu.fiuba.algo3.modelo;
+import java.util;
+public class Jugador {
+    private int energia;
+    private String seniority;
+    private String equipamiento;
+
+    public Jugador(){
+        this.energia = 20;
+        this.seniority = "Novato";
+        this.equipamiento = null;
+    }
+}
