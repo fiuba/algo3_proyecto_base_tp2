5455cbad42bbebcacd31c5676e26cb35243c7825
Nahu <48460903+Nahu2412@users.noreply.github.com>
2024-06-20 18:50:46 -0300

    agregado de los metodos mezclarPreguntas y insertarEnLaLista

--- src/main/java/edu/fiuba/algo3/modelo/GestorDePreguntas.java
+++ src/main/java/edu/fiuba/algo3/modelo/GestorDePreguntas.java
@@ -12,8 +12,7 @@ public class GestorDePreguntas {
 
     public GestorDePreguntas() throws ArchivoInexistente {
         Lector lector = new Lector();
-
-        this.preguntas = lector.cargarPreguntas();
+        this.preguntas = mezclarPreguntas(lector.cargarPreguntas());
     }
 
     public Pregunta obtenerSiguientePregunta() {
@@ -21,4 +20,41 @@ public class GestorDePreguntas {
         contador++;
         return siguientePregunta;
     }
+
+    private List<Pregunta> mezclarPreguntas(List<Pregunta> preguntas){
+        List<Pregunta> otraLista = new ArrayList<>();
+        List<Pregunta> noAgregados = new ArrayList<>();
+        String ultimaTematica = "";
+
+        for(Pregunta pregunta : preguntas){
+            String actualTematica = pregunta.getTematica();
+            if(!actualTematica.equals(ultimaTematica)){
+                ultimaTematica = actualTematica;
+                otraLista.add(pregunta);
+            }else{
+                noAgregados.add(pregunta);
+            }
+        }
+        return insertarEnLaLista(otraLista,noAgregados);
+    }
+
+    private List<Pregunta> insertarEnLaLista(List<Pregunta> listaNormal, List<Pregunta> listaDeNoAgregados){
+        List<Pregunta> listaCopia = listaNormal;
+
+        for(Pregunta preguntaNoAgregada : listaDeNoAgregados){
+            String tematicaActual = preguntaNoAgregada.getTematica();
+            for(int i=1; i<listaCopia.size(); i++){
+                Pregunta anteriorPregunta = listaCopia.get(i-1);
+                Pregunta actualPregunta = listaCopia.get(i);
+                String tematicaDePreguntaAnterior = anteriorPregunta.getTematica();
+                String tematicaDePreguntaActual = actualPregunta.getTematica();
+                if(!tematicaDePreguntaAnterior.equals(tematicaActual) && !tematicaDePreguntaActual.equals(tematicaActual)){
+                    listaCopia.add(i,preguntaNoAgregada);
+                    break;
+                }
+            }
+        }
+        return listaCopia;
+    }
+    
 }
