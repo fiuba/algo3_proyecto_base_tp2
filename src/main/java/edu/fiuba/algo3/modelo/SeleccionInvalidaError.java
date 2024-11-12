83f72a836cf02e90d2b89fb1b33ad24f8cf6ffbd
Zokalyx <zokalyx@gmail.com>
2024-11-12 15:53:25 -0300

    Excepciones

---
+++ src/main/java/edu/fiuba/algo3/modelo/SeleccionInvalidaError.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo;
+
+public class SeleccionInvalidaError extends RuntimeException {
+    public SeleccionInvalidaError(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/SinCartasError.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo;
+
+public class SinCartasError extends RuntimeException {
+    public SinCartasError(String message) {
+        super(message);
+    }
+}
