3bae65b9e5ef235faaf5848b695d8c1b5777015a
agusfffff <mafontana@fi.uba.ar>
2024-11-16 15:17:48 -0300

    parser - ronda - tienda en proceso

---
+++ src/main/java/edu/fiuba/algo3/entrega_2/Tienda.java
@@ -0,0 +1,30 @@
+package edu.fiuba.algo3.entrega_2;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tienda {
+    private List<Comodin> comodines;
+    private List<Tarot> tarots;
+
+    public Tienda(List<Tarot> tarots, List<Comodin> comodines) {
+        this.comodines = comodines;
+        this.tarots = tarots;
+    }
+    public List<Comodin> obtenerComodines() {
+        return comodines;
+    }
+
+    public List<Tarot> obtenerTarots() {
+        return tarots;
+    }
+
+    // supuesto: en la tienda no se repiten cartas especiales
+    public void seleccionar(Comodin comodinSeleccionado) {
+        comodines.removeIf(comodin -> comodin.esElNombre(comodinSeleccionado.getNombre()));
+    }
+
+    public void seleccionar(Tarot tarotSeleccionado) {
+        tarots.removeIf(tarot -> tarot.esElNombre(tarotSeleccionado.getNombre()));
+    }
+
+}
