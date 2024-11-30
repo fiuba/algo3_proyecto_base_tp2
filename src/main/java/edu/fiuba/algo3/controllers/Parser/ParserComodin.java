d91a6bb6e92bcf44244c614d017a46f7148daa00
valentino ceniceros <vceniceros2001@gmail.com>
2024-11-30 20:03:44 -0300

    intentando resolver comodin parserr

--- src/main/java/edu/fiuba/algo3/controllers/Parser/ParserComodin.java
+++ src/main/java/edu/fiuba/algo3/controllers/Parser/ParserComodin.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3.controllers.Parser;
 import com.google.gson.Gson;
 
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import edu.fiuba.algo3.controllers.Factory.ActivacionFactory;
 import edu.fiuba.algo3.controllers.Factory.EfectoFactory;
@@ -15,6 +16,7 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 public class ParserComodin {
@@ -23,9 +25,23 @@ public class ParserComodin {
         Gson gson = new Gson();
 
         try (FileReader reader = new FileReader(direccionDeJson)) {
-            Type comodin =  new TypeToken<List<ComodinParseado>>(){}.getType();
-            List<ComodinParseado> comodines = gson.fromJson(reader, comodin);
 
+            Type mapType = new TypeToken<Map<String, JsonObject>>() {}.getType();
+            Map<String, JsonObject> data = gson.fromJson(reader, mapType);
+
+            List<ComodinParseado> comodines = new ArrayList<>();
+
+
+            for (Map.Entry<String, JsonObject> entry : data.entrySet()) {
+
+                JsonObject comodinData = entry.getValue();
+                if (comodinData.has("comodines")) {
+
+                    Type comodinListType = new TypeToken<List<ComodinParseado>>() {}.getType();
+                    List<ComodinParseado> comodinesParciales = gson.fromJson(comodinData.get("comodines"), comodinListType);
+                    comodines.addAll(comodinesParciales);
+                }
+            }
             return comodines;
         } catch (IOException e) {
             throw new ErrorAlParsearJson();
