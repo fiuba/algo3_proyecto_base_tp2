9c2b3e99db548a8d6f58f86249704e8e038e2eb0
agusfffff <mafontana@fi.uba.ar>
2024-12-07 05:23:01 -0300

    balatro

--- src/main/java/edu/fiuba/algo3/vistas/escenas/VistaBalatro.java
+++ src/main/java/edu/fiuba/algo3/vistas/escenas/VistaBalatro.java
@@ -17,7 +17,6 @@ import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
